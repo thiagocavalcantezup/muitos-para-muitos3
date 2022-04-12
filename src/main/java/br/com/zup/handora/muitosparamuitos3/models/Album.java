996d8024bef2c3c14febae133ec754d9fc26a6b4
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-12 09:51:26 -0300

    Add Album entity

---
+++ src/main/java/br/com/zup/handora/muitosparamuitos3/models/Album.java
@@ -0,0 +1,39 @@
+package br.com.zup.handora.muitosparamuitos3.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "albuns")
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    /**
+     * @deprecated Construtor de uso exclusivo do Hibernate
+     */
+    @Deprecated
+    public Album() {}
+
+    public Album(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}
