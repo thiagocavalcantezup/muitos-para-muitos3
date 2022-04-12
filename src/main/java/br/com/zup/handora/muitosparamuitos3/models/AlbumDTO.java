f745052d89fc474d293edb437cc6ebbf00b83499
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-12 09:48:40 -0300

    Add AlbumDTO

---
+++ src/main/java/br/com/zup/handora/muitosparamuitos3/models/AlbumDTO.java
@@ -0,0 +1,28 @@
+package br.com.zup.handora.muitosparamuitos3.models;
+
+import javax.validation.constraints.NotBlank;
+
+public class AlbumDTO {
+
+    @NotBlank
+    private String titulo;
+
+    @NotBlank
+    private String descricao;
+
+    public AlbumDTO() {}
+
+    public AlbumDTO(@NotBlank String titulo, @NotBlank String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+}
